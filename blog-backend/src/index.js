bd1a91cb624887a6104aa252f46bfbae6bd2bd8e
sg-wooyoung <wyseo@seegene.com>
2021-11-02 14:36:30 +0900

    [Add] mongodb 연결

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import api from './api';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = new Koa();
 const router = new Router();
@@ -11,6 +15,18 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to my port 4000');
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to my port %d', port);
 });
