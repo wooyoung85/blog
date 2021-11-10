79cb9c307f6fdda4bcd359e06b9c5c731b38c566
wooyoung85 <cerealtigerpower@gmail.com>
2021-11-10 13:18:21 +0900

    [Add] redux 적용

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import rootReducer from 'modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
