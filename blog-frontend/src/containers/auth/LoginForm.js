375470fd287aee11f66281fc0587da83d57fa761
wooyoung85 <cerealtigerpower@gmail.com>
2021-11-16 09:48:07 +0900

    [Add] 로그인, 로그아웃 에러 처리

--- blog-frontend/src/containers/auth/LoginForm.js
+++ blog-frontend/src/containers/auth/LoginForm.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
-import { changeField, initializeForm } from 'modules/auth';
+import React, { useEffect, useState } from 'react';
+import { changeField, initializeForm, login } from 'modules/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from 'components/auth/AuthForm';
+import { check } from 'modules/user';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const [error, setError] = useState();
+
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -21,11 +29,36 @@ const LoginForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password } = form;
+    if ([username, password].includes('')) {
+      setError('빈 칸을 모두 입력하세요.');
+      return;
+    }
+    dispatch(login({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('오류발생');
+      console.log(authError);
+      setError('로그인 실패');
+      return;
+    }
+    if (auth) {
+      console.log('로그인 성공');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [navigate, user]);
 
   return (
     <AuthForm
@@ -33,6 +66,7 @@ const LoginForm = () => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,14 +1,19 @@
+import React, { useEffect, useState } from 'react';
 import { changeField, initializeForm, register } from 'modules/auth';
-import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from 'components/auth/AuthForm';
+import { check } from 'modules/user';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const [error, setError] = useState();
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
     authError: auth.authError,
+    user: user.user,
   }));
 
   const onChange = (e) => {
@@ -25,7 +30,16 @@ const RegisterForm = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    if ([username, password, passwordConfirm].includes('')) {
+      setError('빈 칸을 모두 입력하세요.');
+      return;
+    }
     if (password !== passwordConfirm) {
+      setError('비밀번호가 일치하지 않습니다.');
+      dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+      dispatch(
+        changeField({ form: 'register', key: 'passwordConfirm', value: '' }),
+      );
       return;
     }
     dispatch(register({ username, password }));
@@ -39,13 +53,27 @@ const RegisterForm = () => {
     if (authError) {
       console.log('오류발생');
       console.log(authError);
+      if (authError.response.status === 409) {
+        setError('이미 존재하는 계정명입니다.');
+        return;
+      }
+      setError('회원가입 실패');
       return;
     }
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
+      dispatch(check());
     }
-  }, [auth, authError]);
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      console.log('check API 성공');
+      console.log(user);
+      navigate('/');
+    }
+  }, [navigate, user]);
 
   return (
     <AuthForm
@@ -53,6 +81,7 @@ const RegisterForm = () => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
