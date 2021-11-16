e62bda861a088d9349d82b26aa57d12537759432
wooyoung85 <cerealtigerpower@gmail.com>
2021-11-16 21:52:20 +0900

    [Add] Quill Editor 추가

---
+++ blog-frontend/src/components/write/Editor.js
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react';
+import Responsive from 'components/common/Responsive';
+import palette from 'lib/styles/palette';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import styled from 'styled-components';
+
+const EditorBlock = styled(Responsive)`
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+
+const TiltleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[4]};
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor.ql-black::before {
+    left: 0px;
+  }
+`;
+
+const Editor = () => {
+  const quillElement = useRef(null);
+  const quillInstance = useRef(null);
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      modules: {
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+  }, []);
+
+  return (
+    <EditorBlock>
+      <TiltleInput placeholder="제목을 입력하세요" />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlock>
+  );
+};
+
+export default Editor;
